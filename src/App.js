2602a7e6342ce39e0f59f8b23410f94ddcbc4978
KhangTT <Khangtt@rikkeisoft.com>
2022-07-04 14:14:10 +0700

    Commit add router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import { Button } from 'semantic-ui-react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from '~/components/Header/Header';
+import Home from './components/Home/Home';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
         <div className="App">
-            <Button>Hello</Button>
+            <Router>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/movie/:imdbID" element={<MovieDetail />} />
+                    <Route element={<PageNotFound />} />
+                </Routes>
+                <Footer />
+            </Router>
         </div>
     );
 }
